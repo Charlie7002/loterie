250cc0c04ebcd3af0325fdf287d4cd73653bb72b
chca <cachet.cha@gmail.com>
2023-04-04 23:19:06 +0200

    create contract

---
+++ contracts/Raffle.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle {
+    /*State variable*/
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /*Events*/
+    event RaffleEnter(address indexed played);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords() public {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
